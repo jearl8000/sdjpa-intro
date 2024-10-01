271154b70b246fc4ca8d6ebf01a4b3f33c3e1795
jearl <jearl@collegenet.com>
2024-10-01 12:20:01 -0700

    autoincrement tables, add H2-specific migration

--- src/main/java/guru/springframework/sdjpaintro/domain/Author.java
+++ src/main/java/guru/springframework/sdjpaintro/domain/Author.java
@@ -7,7 +7,7 @@ import java.util.*;
 @Entity
 public class Author {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String firstName;
